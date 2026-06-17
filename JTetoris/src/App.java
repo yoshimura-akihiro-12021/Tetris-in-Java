85bf4456c47c8794fd957963a5fdccafb13259e0
yoshimura-akihiro-12021 <712-student032@la-study.com>
2026-06-17 09:07:38 +0900

    保存

--- JTetoris/src/App.java
+++ JTetoris/src/App.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 
 
 public class App {
+    
     public static void main(String[] args) throws Exception {
 
         // JFrameを用いてウィンドウを作成
@@ -9,7 +10,6 @@ public class App {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 600);
         frame.setVisible(true);
-
         // Jpanelを用いてゲーム画面を作成
         // ゲーム画面のサイズを設定
         GamePanel gamePanel = new GamePanel();
--- JTetoris/src/GamePanel.java
+++ JTetoris/src/GamePanel.java
@@ -1,23 +1,28 @@
+
 public class GamePanel extends javax.swing.JPanel {
 
     int bx = 0; // ブロックのx座標
     int by = 0; // ブロックのy座標
+
     // タイマー（ループ）を仕込む
     // コンストラクタの中でタイマーを作成し、ループさせる
     // Processingで言うvoid setup()の中でタイマーを作成するイメージ
 
+    // Prosessingで言うvoid drow()箇所
     public GamePanel() {
+
         javax.swing.Timer timer = new javax.swing.Timer(1000, e -> {
             // ここにループさせたい処理を書く
             // 今は1000ミリ秒ごとに画面を再描画
             bx++; // ブロックのx座標を1増やす
             by++; // ブロックのy座標を1増やす
+            System.out.println("x:" + bx + " y:" + by);
+            System.out.println(" w:" + getWidth() + " h:" + getHeight());
             repaint(); // 画面を再描画する
         });
         timer.start();
     }
 
-    // Prosessingで言うvoid drow()
     public void paintComponent(java.awt.Graphics g) {
         super.paintComponent(g);
         // ゲーム画面の背景を黒に設定
